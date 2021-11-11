e4754cecb96dc39c6ba15b9304ec56848bd8217a
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-11-10 19:38:03 -0600

    Distribucion de archivos en carpetas

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/app.dart';
+
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text("Hola Mundo")));
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Center(child: Text("Hola Mundo")));
+  }
+}

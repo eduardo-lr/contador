da94c28762c0c2573b66f98ef6c7d82c3f206536
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-11-11 14:55:14 -0600

    Botón flotante agregado

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,20 +5,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final estiloTexto = TextStyle(fontSize: 25);
 
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Titulo"),
-      ),
+      appBar: AppBar(title: Text("Titulo")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Numero de clicks:", style: estiloTexto),
-            Text("0", style: estiloTexto)
-          ],
-        ),
-      ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("Numero de clicks:", style: estiloTexto),
+        Text("0", style: estiloTexto)
+      ])),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Mensaje de prueba");
+          },
+          child: Icon(Icons.add)),
     );
   }
 }

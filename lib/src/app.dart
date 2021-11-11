c17d4d614b324d9b316a5a0ecb1adbcb50981fb7
Eduardo Lara Ramirez <eduardo.larar@sat.gob.mx>
2021-11-10 19:55:35 -0600

    Scallfold agregado, la app no corre

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:contador/src/pages/home_page.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text("Hola Mundo")));
+    return MaterialApp(home: Center(child: HomePage()));
   }
 }
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+      ),
+      body: Center(
+        child: Text("Hola Mundo!"),
+      ),
+    );
+  }
+}
